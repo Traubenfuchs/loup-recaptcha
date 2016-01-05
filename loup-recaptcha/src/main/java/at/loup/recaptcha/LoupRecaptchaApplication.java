927e461966f2e2d996b6bfc82bd812b56ccfdd9c
Traubenfuchs <akkira@live.at>
2016-01-05 17:35:22 +0100

    update

--- loup-recaptcha/src/main/java/at/loup/recaptcha/LoupRecaptchaApplication.java
+++ loup-recaptcha/src/main/java/at/loup/recaptcha/LoupRecaptchaApplication.java
@@ -3,7 +3,6 @@ package at.loup.recaptcha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
