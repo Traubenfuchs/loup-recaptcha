785b6cbad9cd0929ee9b99d00c72a8c8fbc2c7fc
Traubenfuchs <akkira@live.at>
2016-01-05 17:55:23 +0100

    update

--- loup-recaptcha/src/main/java/at/loup/recaptcha/LoupRecaptchaApplication.java
+++ loup-recaptcha/src/main/java/at/loup/recaptcha/LoupRecaptchaApplication.java
@@ -2,15 +2,22 @@ package at.loup.recaptcha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "at.loup" })
-@EnableAspectJAutoProxy(proxyTargetClass=true)
-public class LoupRecaptchaApplication {
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+public class LoupRecaptchaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoupRecaptchaApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LoupRecaptchaApplication.class);
 	}
 }
